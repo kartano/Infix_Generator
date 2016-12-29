5669c58cdc3e67eaf0fa0c55ff4eb1002c829bfc
Simon Mitchell <kartano@gmail.com>
2016-12-29 16:42:25 -0600

    Complete rebuild
    Rebuild based on sample from web.  Handles more operators and handles
    left/right operator association correctly.

--- infix_to_postfix.php
+++ infix_to_postfix.php
@@ -1,108 +1,171 @@
 <?php
 /**
- * Infix to postfix expression convertor.
+ * Infix to postfix generator and evaluator
  *
+ * @author          https://www.rune-server.ee/members/funke/
  * @author          Simon Mitchell <simon.mitchell@evgo.com>
- * @version         1.0.0               2016-12-20 14:39:13 SM:  Prototype
- * @version         1.1.0               2016-12-21 17:29:51 SM:  Fixed problem with unbounded loops.
+ * @see             https://www.rune-server.ee/programming/website-development/446325-php-calculator-infix-postfix-eval.html
+ * @version         2.0.0               2016-12-29 16:39:18 SM:  Prototype
  */
 
-$txtExpr="17.3*(1+2)-(173.23+5*(23+11.2312))+26";
-$txtOriginal=$txtExpr;
-$lngCounter=0;
+define('ASSOC_NONE', 0);
+define('ASSOC_LEFT', 1);
+define('ASSOC_RIGHT', 2);
 
-// SM:  Operator precedence:  Lowest to highest.
-$arrOperatorPrecedence=array('-','+','*','/');
-
-$arrOutputStack=array();
-$arrOperatorStack=array();
-
-$txtExpr=preg_replace('/\s/','',$txtExpr);
-while (true)
+/**
+ * Postfix Utilities Class
+ */
+final class PostfixUtils
 {
-    if ($txtExpr=='')
-        break;    
-    elseif (@preg_match('/^(\d*\.\d+)/',$txtExpr,$arrMatches))
+    protected static $operators = array('^' => array(9, ASSOC_RIGHT, false),
+					'*' => array(8, ASSOC_LEFT, false),
+					'/' => array(8, ASSOC_LEFT, false),
+					'%' => array(8, ASSOC_LEFT, false),
+					'+' => array(5, ASSOC_LEFT, false),
+					'-' => array(5, ASSOC_LEFT, false),
+					'(' => array(0, ASSOC_NONE, false),
+					')' => array(0, ASSOC_NONE, false));
+
+    private static function precedence($opchar)
     {
-        echo "<p>Float: {$arrMatches[0]}";
-        $txtExpr=substr($txtExpr,strlen($arrMatches[0])-strlen($txtExpr));
-        $arrOutputStack[]=$arrMatches[0];
-        if (strlen($txtExpr)==strlen($arrMatches[0]))
-            break;        
-    }    
-    elseif (@preg_match('/^[0-9]+/',$txtExpr,$arrMatches))
-    {
-        echo "<p>Integer: {$arrMatches[0]}";
-        $txtExpr=substr($txtExpr,strlen($arrMatches[0])-strlen($txtExpr));
-        $arrOutputStack[]=$arrMatches[0];
-        if (strlen($txtExpr)==strlen($arrMatches[0]))
-            break;        
+	    return self::$operators[$opchar][0];
     }
-    elseif (@preg_match('/^[+-\/*]/',$txtExpr,$arrMatches))
+
+    private static function associativity($opchar)
     {
-        echo "<p>Operator: {$arrMatches[0]}";
-        $txtExpr=substr($txtExpr,strlen($arrMatches[0])-strlen($txtExpr));
-        if (count($arrOperatorStack==0))
-            $arrOperatorStack[]=$arrMatches[0];
-        else
-        {
-            $txtTopOp=$arrOperatorStack[count($arrOperatorStack)-1];
-            // If the current op has a HIGHER priority that top op, push the current op onto the operator stack.
-            $txtCurrentOpPrecedence=array_search($arrMatches[0],$arrOperatorPrecedence);
-            $txtTopOpPrecedence=array_search($txtTopOp,$arrOperatorPrecedence);
-            if ($txtCurrentOpPrecedence > $txtTopOpPrecedence)
-                $arrOperatorStack[]=$arrMatches[0];
-            else
-            {
-                while (true)
-                {
-                    if (count($arrOperatorStack)==0)
-                        break;
-                    else
-                        $arrOutputStack[]=array_pop($arrOperatorStack);
-                }
-                $arrOperatorStack[]=$arrMatches[0];
-            }
-        }
-        if (strlen($txtExpr)==strlen($arrMatches[0]))
-            break;
+    	return self::$operators[$opchar][1];
     }
-    elseif (@preg_match('/^(\(){1}/',$txtExpr,$arrMatches))
+
+    private static function unary($opchar)
     {
-        echo "<p>Opening Bracket: {$arrMatches[0]}";
-        $txtExpr=substr($txtExpr,1,strlen($txtExpr)-1);
-        $arrOperatorStack[]=$arrMatches[0];
-        if (strlen($txtExpr)==strlen($arrMatches[0]))
-            break;        
+	    return self::$operators[$opchar][2];
     }
-    elseif (@preg_match('/^(\)){1}/',$txtExpr,$arrMatches))
+
+    private static function is_operator($char)
     {
-        echo "<p>Closing Bracket: {$arrMatches[0]}";
-        $txtExpr=substr($txtExpr,1,strlen($txtExpr)-1);
-        $blnFound=false;
-        while (!$blnFound)
-        {
-            $txtOp=array_pop($arrOperatorStack);
-            if ($txtOp=='(')
-                $blnFound=true;
-            else
-                $arrOutputStack[]=$txtOp;
-        }
-        if (strlen($txtExpr)==strlen($arrMatches[0]))
-            break;        
+	    return array_key_exists($char, self::$operators);
     }
-    else
-        break;
+
+    private static function starts_with($haystack, $needle)
+    {
+	    return !strncmp($haystack, $needle, strlen($needle));
+    }
+
+    private static function ends_with($haystack, $needle)
+    {
+	    return substr($haystack, -strlen($needle)) === $needle;
+    }
+
+    private static function array_peek(array $stack)
+    {
+	    return $stack[count($stack) - 1];
+    }
+
+    public static function postfix($expression)
+    {
+        //---------------------------------------------------------------------------------------------
+        // SM:  Remove any white space from the expression.
+        //---------------------------------------------------------------------------------------------
+        
+	    $expression = preg_replace('/\s+/', '', $expression);
+
+        //---------------------------------------------------------------------------------------------
+        // SM:  Make sure out expression is encapsulated within parenthesis.
+        //---------------------------------------------------------------------------------------------
+	
+    	if (!self::starts_with($expression, '('))
+		    $expression = '('.$expression;
+	    if (!self::ends_with($expression, ')'))
+		    $expression .= ')';
+	
+    	$stack = array();
+    	$output = '';
+    	$previous = true;
+	
+    	for ($i = 0; $i < strlen($expression); $i++)
+    	{
+    		$char = $expression[$i];
+    		if (self::is_operator($char))
+    		{
+    			if ($char == '(')
+    				array_push($stack, $char);
+    			else if ($char == ')')
+    			{
+    				while (count($stack) > 0 && ($top = self::array_peek($stack)) != '(')
+    				{
+    					$output .= ' '.$top;
+    					array_pop($stack);
+    				}    				
+    				array_pop($stack);
+    			}
+    			else
+    			{
+    				while (count($stack) > 0)
+    				{
+    					$peek = self::array_peek($stack);
+    					if (self::associativity($char) == ASSOC_LEFT && self::precedence($char) <= self::precedence($peek) 
+    					|| self::associativity($char) == ASSOC_RIGHT && self::precedence($char) < self::precedence($peek))
+    					{
+    						$output .= ' '.self::array_peek($stack);
+    						array_pop($stack);
+    					}
+    					else
+    						break;
+    				}
+    				array_push($stack, $char);
+    			}
+    			$previous = true;
+    		}
+    		else
+    		{
+    			$output .= ($previous ? ' ' : '').$char;
+    			$previous = false;
+    		}
+    	}
+	
+    	while (count($stack) > 0)
+    	{
+    		if (self::array_peek($stack) == '(')
+    			array_pop($stack);
+    		else
+    			$output .= ' '.array_pop($stack);
+    	}	
+    	return $output;
+    }
+
+    public static function postfix_eval($postfix)
+    {
+    	$stack = array();
+    	$num = '';
+    	for ($i = 0; $i < strlen($postfix); $i++)
+    	{
+    		$char = $postfix[$i];
+    		if (self::is_operator($char))
+    		{
+    			$second = array_pop($stack);
+    			if ($char == '^')
+    				array_push($stack, pow(array_pop($stack), $second));
+    			else
+    				array_push($stack, eval("return ".array_pop($stack)." $char $second;"));
+    		}
+    		else
+    		{
+    			if ($char == ' ')
+    			{
+    				if (strlen($num) > 0)
+    					array_push($stack, $num);    				
+    				$num = '';
+    			}
+    			else
+    				$num .= $char;
+    		}
+    	}
+    	return array_pop($stack);
+    }
 }
-while (true)
-{
-    if (count($arrOperatorStack)==0)
-        break;
-    else
-        $arrOutputStack[]=array_pop($arrOperatorStack);
-}
-echo "<pre>";
-print_r($arrOutputStack);
-echo "</pre>";
-echo "<p>The postfix (or 'Polish') notation for {$txtOriginal} is: <b><code>".implode(' ',$arrOutputStack)."</code></b>";
+$expression = '(32*3)+(4/5-(6^9/(4%8)))';
+echo('Expression: '.$expression.'<br />');
+$postfix=PostfixUtils::postfix($expression);
+echo('Postfix: '.$postfix.'<br />');
+echo('Evaluation: '.PostfixUtils::postfix_eval($postfix));
 ?>
